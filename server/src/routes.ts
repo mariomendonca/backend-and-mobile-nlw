db4f35e1f907752305c1fb7ef9e82ec89de213cd
Mario Mendonca <mariomeendonca00@gmail.com>
2020-06-05 14:12:12 -0300

    coding

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,46 @@
 import express from 'express'
+import knex from './database/connection'
+
 const routes = express.Router() //serve para mudar as rotas para outro arquivo
 
+routes.get('/items', async (req, res) => {
+    const items = await knex('items').select('*')
 
-routes.get('/', (req, res) => {
-    return res.json({message:'hello world'})
+    const serializedItems = items.map(item => {
+        return {
+            id:item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
+    })
+
+    return res.json(serializedItems)
+})
+
+routes.post('/points', async (req, res) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = req.body
+
+    await knex('points').insert({
+        image:'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+    })
+
+    return res.json({success: true})
 })
 
 export default routes
