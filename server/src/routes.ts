31f5e3cda54eb6374dd16ee2c605ff7a278c6414
Mario Mendonca <mariomeendonca00@gmail.com>
2020-06-06 22:35:25 -0300

    coding

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,46 +1,15 @@
 import express from 'express'
-import knex from './database/connection'
 
+import PointsController from './controlles/PointsController'
+import ItemsController from './controlles/ItemsController'
+
+//index, show(listar 1), create, update, delete(ou destroy)
 const routes = express.Router() //serve para mudar as rotas para outro arquivo
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
 
-routes.get('/items', async (req, res) => {
-    const items = await knex('items').select('*')
+routes.get('/items', itemsController.index)
+routes.post('/points', pointsController.create)
 
-    const serializedItems = items.map(item => {
-        return {
-            id:item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`
-        }
-    })
-
-    return res.json(serializedItems)
-})
-
-routes.post('/points', async (req, res) => {
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = req.body
-
-    await knex('points').insert({
-        image:'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-    })
-
-    return res.json({success: true})
-})
 
 export default routes
